2d83e5ade2d3163aeb126d71df6e017ba291b56e
Spencer Harston <spencer314@gmail.com>
2025-09-26 22:55:22 -0600

    add option to show recent list of tracks

--- src/assets/scripts/now-playing.js
+++ src/assets/scripts/now-playing.js
@@ -3,6 +3,7 @@ class NowPlaying extends HTMLElement {
   constructor() {
     super();
     this.username = this.getAttribute("username");
+    this.recents = this.getAttribute("recents") || false;
   }
 
   connectedCallback() {
@@ -36,14 +37,15 @@ class NowPlaying extends HTMLElement {
 
   render() {
     this.innerHTML = `
-    <div class="text-sm inline-block" id="now-playing-content">
-      Fetching tracks...
+    <div class="text-sm not-prose flex flex-col gap-y-2" id="now-playing-content">
+      Fetching last.fm data...
     </div>
     `;
   }
 
   async fetchNowPlaying() {
     if (!this.username) {
+      this.showError("Missing username");
       return;
     }
 
@@ -54,45 +56,50 @@ class NowPlaying extends HTMLElement {
       const data = await response.json();
 
       if (data.error) {
-        console.log(data.message);
+        this.showError("API call to last.fm failed, check the console");
         return;
       }
 
-      console.log(data);
+      const content = this.querySelector("#now-playing-content");
 
       // check for list of tracks is available
       const tracks = data.recenttracks?.track;
       if (!tracks || tracks.length === 0) {
-        this.showNotPlaying();
+        content.innerHTML = this.getNotPlayingElement();
+        return;
       }
 
       // get the first track in the list
       const track = Array.isArray(tracks) ? tracks[0] : tracks;
       const isNowPlaying = track["@attr"]?.nowplaying === "true";
 
-      // update if track has changed to avoid unnecessary DOM updates
+      // update only if track has changed to avoid unnecessary DOM updates
       const trackId = `${track.name}-${track.artist["#text"] || track.artist}`;
       if (this.currentTrackId !== trackId || this.isPlaying !== isNowPlaying) {
         this.currentTrackId = trackId;
         this.isPlaying = isNowPlaying;
-        this.showTrack(track, isNowPlaying);
+
+        const nowplayingEl = this.getNowPlayingElement(track, isNowPlaying);
+        content.innerHTML = nowplayingEl;
+
+        if (this.recents) {
+          const recentTrackEl = this.getRecentTracksElement(tracks.slice(0, 5));
+          content.innerHTML += recentTrackEl;
+        }
       }
     } catch (error) {
       console.error(error);
+      this.showError("Failed to load last.fm data");
     }
   }
 
-  showTrack(track, isNowPlaying) {
-    console.log(`updating track`);
-    const content = this.querySelector("#now-playing-content");
+  getNowPlayingElement(track, isNowPlaying) {
     const icon = `<svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="icon icon-tabler icons-tabler-outline icon-tabler-music"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M3 17a3 3 0 1 0 6 0a3 3 0 0 0 -6 0" /><path d="M13 17a3 3 0 1 0 6 0a3 3 0 0 0 -6 0" /><path d="M9 17v-13h10v13" /><path d="M9 8h10" /></svg>`;
     const status = isNowPlaying ? "Now Playing" : "Last Played";
 
-    const albumArt = track.image[2]["#text"]; // TODO: does last.fm provide fallback art if there is no artwork?
-
-    content.innerHTML = `
+    const nowPlayingElement = `
     <div class="flex gap-x-3 items-center">
-      <img width="72" height="72" src="${albumArt}" title="Album art for ${this.escapeHtml(track.album["#text"])}">
+      ${this.albumArtElement(track, 72)}
       <div>
         <div class="gap-x-1 inline-flex items-center">${icon} ${status}</div>
         <div class="font-bold">${this.escapeHtml(track.name)}</div>
@@ -100,12 +107,63 @@ class NowPlaying extends HTMLElement {
       </div>
     </div>
     `;
+
+    return nowPlayingElement;
   }
 
-  showNotPlaying() {
+  getRecentTracksElement(tracks) {
+    const tracksElements = tracks.map((t) => this.trackAndAlbumListItem(t)).join("");
+    const recentTracksElement = `
+    <details>
+      <summary>Recent Tracks</summary>
+      <ul class="ml-2">
+        ${tracksElements}
+      </ul>
+    </details>
+    `;
+    return recentTracksElement;
+  }
+
+  getNotPlayingElement() {
+    return `<div>Nothing playing right now</div>`;
+  }
+
+  trackAndAlbumListItem(track) {
+    return `
+    <li class="flex my-2 gap-x-3 items-center">
+      ${this.albumArtElement(track, 48)}
+      <div>
+        <div class="font-bold">${this.escapeHtml(track.name)}</div>
+        <div class="">${this.escapeHtml(track.artist["#text"] || track.artist)}</div>
+      </div>
+    </li>
+    `;
+  }
+
+  albumArtElement(track, size) {
+    function getImageIndex(size) {
+      const availableSizes = [34, 64, 174, 300];
+      const index = availableSizes.findIndex((imageSize) => imageSize > size);
+      return index !== -1 ? index : availableSizes.length - 1;
+    }
+
+    const imgElement = document.createElement("img");
+    imgElement.src = track.image[getImageIndex(size)]["#text"] || this.albumArtFallback();
+    imgElement.alt = `Album art for ${this.escapeHtml(track.album["#text"] || track.name)}`;
+    imgElement.width = size;
+    imgElement.height = size;
+
+    return imgElement.outerHTML;
+  }
+
+  albumArtFallback() {
+    return `data:image/svg+xml;utf8,<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="icon icon-tabler icons-tabler-outline icon-tabler-disc"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M12 12m-9 0a9 9 0 1 0 18 0a9 9 0 1 0 -18 0" /><path d="M12 12m-1 0a1 1 0 1 0 2 0a1 1 0 1 0 -2 0" /><path d="M7 12a5 5 0 0 1 5 -5" /><path d="M12 17a5 5 0 0 0 5 -5" /></svg>`;
+  }
+
+  showError(message) {
     const content = this.querySelector("#now-playing-content");
     content.innerHTML = `
-      <div>Nothing playing right now</div>
+      <div class="text-red-500">⚠️ ${this.escapeHtml(message)}</div>
     `;
   }
 
